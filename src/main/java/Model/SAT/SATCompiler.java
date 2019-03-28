65b452809c048cd146917b99a5d251fb067c84f9
Rotem-Lev-Lehman <rotem12430@gmail.com>
2019-03-28 17:43:03 +0200

    sat

---
+++ src/main/java/Model/SAT/SATCompiler.java
@@ -0,0 +1,17 @@
+package Model.SAT;
+
+import Model.Apartment;
+import Model.Student;
+
+public class SATCompiler {
+
+    public void addApartment(Apartment apartment){
+
+    }
+
+    public void addStudent(Student student){
+
+    }
+
+
+}
